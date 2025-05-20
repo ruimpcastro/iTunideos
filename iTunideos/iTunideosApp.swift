394047c836ff8f4737aa39e3301b23e54c4e009d
Rui Castro <rui_castro@live.com.pt>
2025-05-20 12:48:42 +0000

    Initial Commit

---
+++ iTunideos/iTunideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunideosApp.swift
+//  iTunideos
+//
+//  Created by Rui Castro on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct iTunideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
